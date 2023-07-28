f5b35cca9a3d4f4542c06f6cb46bedfa29127ee9
philipmjohnson <philipmjohnson@users.noreply.github.com>
2023-07-28 17:49:04 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,3 @@
+moreaEventData = [
+  {title: "E16: Experience Local Portfolio Management", url: "/archive-2022-2023/morea/configuration-management/experience-local-portfolio.html", start: "2022-02-01T09:00"}
+]
